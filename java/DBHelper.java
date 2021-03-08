799d05fbf9a256124c7e4f4472d2af6f69ae2de0
syoung622 <68267278+syoung622@users.noreply.github.com>
2021-03-08 15:23:48 +0900

    Add files via upload

--- java/DBHelper.java
+++ java/DBHelper.java
@@ -1,17 +1,21 @@
 package com.example.dailyeco;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 
 import org.w3c.dom.Text;
 
-import java.text.DateFormat;
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -26,47 +30,72 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        if (!db.isReadOnly()) {
+        // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+
         //데이터베이스 생성될 때 호출
         // 데이터베이스 -> 테이블 -> 컬럼 -> 값
 
-        //user정보 테이블
-        db.execSQL("CREATE TABLE IF NOT EXISTS tbl_user (id_user INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name_user TEXT, email_user TEXT, passwd_user TEXT)");
-
-        db.execSQL("INSERT into tbl_user\n" +
-                "(name_user,email_user,passwd_user)\n" +
-                "VALUES('장효신','ewha1234@gmail.com','ewha123!$');");
         //daily 기록 테이블
-        db.execSQL("CREATE TABLE IF NOT EXISTS tbl_dailynum (id_dailynum INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, doc_date DATE, id_goal INTEGER, daily_count INTEGER(10), id_user INTEGER, FOREIGN KEY(id_goal) REFERENCES tbl_goal(id_goal), FOREIGN KEY(id_user) REFERENCES tbl_user(id_user))");
+        db.execSQL("CREATE TABLE IF NOT EXISTS tbl_dailynum (id_dailynum INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, doc_date TEXT, id_goal INTEGER, daily_count INTEGER(10), FOREIGN KEY(id_goal) REFERENCES tbl_goal(id_goal))");
 
 
         db.execSQL("INSERT into tbl_dailynum\n" +
-                "(doc_date,id_goal,daily_count,id_user)\n" +
-                "VALUES('2021-02-24',1,6,1);");
+                "(doc_date,id_goal,daily_count)\n" +
+                "VALUES('2021-03-24',1,6);");
 
         //환경실천목표 테이블
-        db.execSQL("CREATE TABLE IF NOT EXISTS tbl_goal (id_goal INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name_goal TEXT, id_user INTEGER, is_active BOOLEAN, FOREIGN KEY(id_user) REFERENCES tbl_user(id_user))");
+        db.execSQL("CREATE TABLE IF NOT EXISTS tbl_goal (id_goal INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name_goal TEXT, icon_goal BLOB, is_active INTEGER)");
 
-        db.execSQL("INSERT INTO tbl_goal (name_goal, id_user, is_active) VALUES" +
-                "('텀블러 사용하기',1,true)," +
-                "('장바구니 사용하기',1,true)," +
-                "('배달 시 일회용 수저, 포크 받지 않기',1,true)," +
-                "('재활용 용기 씻어서 버리기',1,true)," +
-                "('외식할 때 잔반 남기지 않기',1,true)," +
-                "('가까운 층은 걸어서 올라가기',1,true)," +
-                "('안쓰는 플러그 뽑기',1,false)," +
-                "('양치컵 사용하기',1,false)," +
-                "('종이 영수증 받지 않기',1,false)," +
-                "('페트병 뚜껑, 링, 비닐 분리해서 버리기',1,false)");
+        Insert_tbl_goal("텀블러 사용하기",iconBitmap(R.drawable.ic_1),1);
+        Insert_tbl_goal("장바구니 사용하기",iconBitmap(R.drawable.ic_2),1);
+        Insert_tbl_goal("배달 시 일회용 수저, 포크 받지 않기",iconBitmap(R.drawable.ic_3),1);
+        Insert_tbl_goal("재활용 용기 씻어서 버리기",iconBitmap(R.drawable.ic_4),1);
+        Insert_tbl_goal("외식할 때 잔반 남기지 않기",iconBitmap(R.drawable.ic_5),1);
+        Insert_tbl_goal("가까운 층은 걸어서 올라가기",iconBitmap(R.drawable.ic_6),1);
+        Insert_tbl_goal("안쓰는 플러그 뽑기",iconBitmap(R.drawable.ic_7_7),0);
+        Insert_tbl_goal("양치컵 사용하기",iconBitmap(R.drawable.ic_8_8),0);
+
+/*
+        db.execSQL("INSERT INTO tbl_goal (name_goal, icon_goal, is_active) VALUES" +
+                "('장바구니 사용하기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1'),"+
+                "('배달 시 일회용 수저, 포크 받지 않기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1')," +
+                "('재활용 용기 씻어서 버리기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1')," +
+                "('외식할 때 잔반 남기지 않기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1')," +
+                "('가까운 층은 걸어서 올라가기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1')," +
+                "('안쓰는 플러그 뽑기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1')," +
+                "('양치컵 사용하기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1')," +
+                "('종이 영수증 받지 않기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1')," +
+                "('페트병 뚜껑, 링, 비닐 분리해서 버리기'+', '+'"+iconBitmap(R.drawable.ic_1)+"'+', '+'1')");
+
+ */
+
 
         //목표횟수변경 테이블
-        db.execSQL("CREATE TABLE IF NOT EXISTS tbl_goalcount (id_count INTEGER PRIMARY KEY AUTOINCREMENT, green_count INTEGER NOT NULL, yellow_count INTEGER NOT NULL, id_user INTEGER, FOREIGN KEY(id_user) REFERENCES tbl_suer(id_user))");
+        db.execSQL("CREATE TABLE IF NOT EXISTS tbl_goalcount (id_count INTEGER PRIMARY KEY AUTOINCREMENT, green_count INTEGER NOT NULL, yellow_count INTEGER NOT NULL)");
 
         db.execSQL("INSERT into tbl_goalcount(\n" +
-                "green_count, yellow_count,id_user)\n" +
-                "VALUES(5,3,1);");
+                "green_count, yellow_count)\n" +
+                "VALUES(5,3);");
 
 
     }
+
+    public byte[] iconBitmap(int d){
+        Drawable drawable = ResourcesCompat.getDrawable(Resources.getSystem(),d,null);
+        Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+        ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,byteArray);
+        byte[] img;
+        img = byteArray.toByteArray();
+
+        return img;
+    }
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -75,14 +104,13 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     //daily 기록 select문
-    //public ArrayList<Dailylist> dailylists = new ArrayList<>();
-    public ArrayList<Dailylist> getDailylist(Date date, int id_user) {
+    public ArrayList<Dailylist> getDailylist(String date) {
         ArrayList<Dailylist> dailylists = new ArrayList<>();
 
         SQLiteDatabase db = getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM TBL_DAILYNUM "+
                 "JOIN TBL_GOAL ON TBL_DAILYNUM.ID_GOAL = TBL_GOAL.ID_GOAL "+
-                "WHERE DOC_DATE "+ date +"AND ID_USER="+id_user,null);
+                "WHERE DOC_DATE "+date,null);
 
 
         if(cursor.getCount()!=0){
@@ -95,7 +123,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
                 Dailylist dailylist = new Dailylist();
                 dailylist.setName_goal(name_goal);
-                dailylist.setDaily_count(Integer.toString(daily_count));
+                dailylist.setDaily_count(daily_count);
 
                 sum+=daily_count;
                 dailylist.setTotal_count(sum);
@@ -111,37 +139,33 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
     //daily 기록 insert문
-    public void Insert_tbl_dailynum(int _id_dailynum, int _daily_num, int _id_user){
+    public void Insert_tbl_dailynum(int _id_dailynum, int _daily_num){
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("INSERT INTO tbl_dailynum (id_dailynum, daily_num, id_user) VALUES('"+_id_dailynum+"', '"+_daily_num+"', '"+_id_user+"')");
+        db.execSQL("INSERT INTO tbl_dailynum (id_dailynum, daily_num) VALUES('"+_id_dailynum+"', '"+_daily_num+"')");
 
     }
 
-    //user정보 insert문
-    public void Insert_tbl_user(int _id_user, String _name_user, String _email_user, String _passwd_user){
-        SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("INSERT INTO tbl_user (id_user, name_user, email_user, passwd_user) VALUES('"+_id_user+"', '"+_name_user+"', '"+_email_user+"', '"+_passwd_user+"')");
-
-    }
 
     //목표횟수를 수정하는 update문
-    public void Update_tbl_goalcount(int _green_count, int _yellow_count, int _id_user){
+    public void Update_tbl_goalcount(int _green_count, int _yellow_count){
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("UPDATE tbl_goalcount SET green_count = '"+_green_count+"', yellow_count = '"+_yellow_count+"' WHERE id_user='"+_id_user+"'" );
+        db.execSQL("UPDATE tbl_goalcount SET green_count = '"+_green_count+"', yellow_count = '"+_yellow_count+"'" );
 
     }
 
+
+    //환경실천목표 테이블에서 icon은 iconBitmap 함수를 이용해서 byte 형식으로 입력
     //환경실천목표를 추가하는 insert문
-    public void Insert_tbl_goal(String _name_goal, int _id_user, boolean _is_active){
+    public void Insert_tbl_goal(String _name_goal, byte[] _icon_goal, int _is_active){
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("INSERT INTO tbl_goal (name_goal, id_user, is_active) VALUES ('"+_name_goal+"','"+_id_user+"'+'"+_is_active+"')");
+        db.execSQL("INSERT INTO tbl_goal (name_goal, icon_goal, is_active) VALUES ('"+_name_goal+"', '"+_icon_goal+"','"+_is_active+"')");
 
     }
 
     //환경실천목표 추가화면의 6개 리스트를 수정하는 update문
-    public void Update_tbl_goal(String _name_goal, int _id_user, boolean _is_active){
+    public void Update_tbl_goal(String _name_goal, byte[] _icon_goal, int _is_active){
         SQLiteDatabase db = getReadableDatabase();
-        db.execSQL("UPDATE tbl_goal SET is_active='"+_is_active+"' WHERE id_user = '"+_id_user+"' AND name_goal = '"+_name_goal+"'");
+        db.execSQL("UPDATE tbl_goal SET is_active='"+_is_active+"', _icon_goal='"+_icon_goal+"' WHERE name_goal = '"+_name_goal+"'");
     }
 
 
