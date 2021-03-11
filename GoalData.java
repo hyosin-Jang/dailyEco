a4353c4d008b893473d461dbfe8073f3764306e6
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2021-03-11 19:57:58 +0900

    Update GoalData.java

--- GoalData.java
+++ GoalData.java
@@ -2,10 +2,14 @@ package com.example.edit_goal2;
 
 public class GoalData {
 
-        private String goal;
 
-    public GoalData(String goal) {
+        private String goal;
+        private int icon;
+
+
+    public GoalData(String goal, int icon) {
         this.goal = goal;
+        this.icon = icon;
     }
 
     public String getGoal() {
@@ -14,5 +18,14 @@ public class GoalData {
 
     public void setGoal(String goal) {
         this.goal = goal;
+    }
+
+    public int getIcon()
+    {
+        return icon;
+    }
+    public void setIcon(int icon)
+    {
+        this.icon = icon;
     }
 }
