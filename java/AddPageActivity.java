68b07938dd9415a93c805f76e30ef8facfd218db
hyosin-Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-03-24 05:07:35 +0900

    Update AddPageActivity.java

--- java/AddPageActivity.java
+++ java/AddPageActivity.java
@@ -1,50 +1,34 @@
-
 package com.example.dailyeco;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.Activity;
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-import java.io.ByteArrayInputStream;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
-import java.util.List;
 
 public class AddPageActivity extends AppCompatActivity {
-    
+
     private Adapter Adapter;
     private RecyclerView recyclerView;
     private GridLayoutManager GridLayout;
     private ImageView icon;
     private TextView text;
     private int number=0;
-    
+
     private SQLiteDatabase database;
     private DBHelper dbHelper;
 
@@ -52,7 +36,7 @@ public class AddPageActivity extends AppCompatActivity {
 
     // *image 파일 - byte[] 변경
     public byte[] iconBitmap(int d) {
-        
+
         Drawable drawable = ContextCompat.getDrawable(this,d);
         Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
         ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
@@ -62,7 +46,7 @@ public class AddPageActivity extends AppCompatActivity {
 
         return img;
     }
-    
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,8 +62,8 @@ public class AddPageActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler1); // 리사이클러뷰 참조
         text = (TextView) findViewById(R.id.num);
         icon = (ImageView) findViewById(R.id.icon);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(myToolbar);
 
         //그리드 레이아웃, 한 행에 2개씩 나타내기로 결정
         GridLayout = new GridLayoutManager(this, 2);
@@ -119,50 +103,72 @@ public class AddPageActivity extends AppCompatActivity {
 
     }
 
-        // *select문으로 데이터 가져오기
-        public void selectData(String tableName){
-            if(database != null){
-                String sql = "SELECT name_goal, icon_goal FROM "+ tableName +" WHERE is_active=1";
-                Cursor cursor = database.rawQuery(sql,null);
+    // *select문으로 데이터 가져오기
+    public void selectData(String tableName){
+        if(database != null){
+            String sql = "SELECT id_goal, name_goal, icon_goal FROM "+ tableName +" WHERE is_active=1";
+            Cursor cursor = database.rawQuery(sql,null);
 
-                for(int i = 0 ; i <6 ; i++){
-                    cursor.moveToNext(); // 다음 레코드로 넘어감
-                    String name = cursor.getString(cursor.getColumnIndex("name_goal")); // name_goal 칼럼 뽑아줌
-                    byte [] Icon = cursor.getBlob(cursor.getColumnIndex("icon_goal")); // icon 칼럼
+            for(int i = 0 ; i <6 ; i++){
+                cursor.moveToNext(); // 다음 레코드로 넘어감
+                int id = cursor.getInt(cursor.getColumnIndex("id_goal")); // id_goal 칼럼 뽑아줌
+                String name = cursor.getString(cursor.getColumnIndex("name_goal")); // name_goal 칼럼 뽑아줌
+                byte [] Icon = cursor.getBlob(cursor.getColumnIndex("icon_goal")); // icon 칼럼
 
-                    // * byte[] => drawable 로 typecasting
-                    Drawable drawable = new BitmapDrawable(getResources(),BitmapFactory.decodeByteArray(Icon,0,Icon.length));
-                    // 리사이클러뷰에 추가
-                    addItem(drawable,"0",name);
-                }
-                cursor.close(); // cursor도 데이터베이스 접근하는 것이기 때문에 꼭 닫아준다.
+                // * byte[] => drawable 로 typecasting
+                //Drawable drawable = new BitmapDrawable(getResources(),BitmapFactory.decodeByteArray(Icon,0,Icon.length));
+                //Bitmap ic = bytes2Bitmap(Icon);
+
+                //디폴트 drawable
+                Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_7, null);
+
+                if (id ==1){
+                drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_1, null);}
+                else if (id ==2){
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_2, null);}
+                else if (id ==3){
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_3, null);}
+                else if (id ==4){
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_4, null);}
+                else if (id ==5){
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_5, null);}
+                else if (id ==6){
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_6, null);}
+                else
+                { // 그밖의 것들
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_7, null);}
+
+                // 리사이클러뷰에 추가
+                addItem(drawable,"0",name);
+                Adapter.notifyDataSetChanged();
             }
+            cursor.close(); // cursor도 데이터베이스 접근하는 것이기 때문에 꼭 닫아준다.
         }
+    }
 
-        /*
-        // 스트링을 바이너리 바이트 배열로
-        public static byte[] binaryStringToByteArray(String s){
-            int count = s.length()/8;
-            byte [] b = new byte[count];
-            for(int i =1 ; i<count; i++){
-                String t = s.substring((i-1)*8,i*8);
-                b[i-1] = binaryStringToByte(t);
-            }
-            return b;
-        }
+/*
+    public static Drawable bytes2Drawable(byte[] b) {
+        Bitmap bitmap = bytes2Bitmap(b);
+        return bitmap2Drawable(bitmap);
+    }// ww  w . ja v a  2 s. c o m
 
-        // 스트링을 바이너리 바이트로
-        public static byte binaryStringToByte(String s){
-            byte ret =0,total=0;
-            for(int i=0 ; i<8 ; i++){
-                ret = (s.charAt(7-i) =='1')?(byte)(1<<i):0;
-                total = (byte)(ret|total);
-            }
-            return total;
-        }
+    public static Bitmap bytes2Bitmap(byte[] b) {
+        if (b.length != 0) {
+            return BitmapFactory.decodeByteArray(b, 0, b.length);}
+        return null;
+    }
+    public static Drawable bitmap2Drawable(Bitmap bitmap) {
+        @SuppressWarnings("deprecation")
+        BitmapDrawable bd = new BitmapDrawable(bitmap);
+        Drawable d = (Drawable) bd;
+        return d;
+    }
+    // ** byte [] => int
+    public int byteToint(byte[] arr){ return (arr[0] & 0xff)<<24 |
+            (arr[1] & 0xff)<<16 | (arr[2] & 0xff)<<8 | (arr[3] & 0xff); }
 
-        */
-    
+*/
+
     public void addItem(Drawable icon, String num, String content) {
         ListViewItem item = new ListViewItem();
         item.setIcon(icon);
